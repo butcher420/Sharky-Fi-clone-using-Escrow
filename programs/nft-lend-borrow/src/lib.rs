74b159fa1f19cf00cbac4ee614a218bdf629415c
butcher420 <CroffieFrens251@gmail.com>
2024-07-26 09:29:11 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("6naAJsXV1tCRkncvt3BFUtq7ygU9KRPF1CrTYsz1vMfZ");
 
 #[program]
 pub mod nft_lend_borrow {
